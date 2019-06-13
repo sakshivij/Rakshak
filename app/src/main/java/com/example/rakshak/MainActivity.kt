8483eefe98b6c0630cc25c79cfc229feabc85b3c
sakshivij <sakshivij12@gmail.com>
2019-06-14 02:00:54 +0530

    initial commit

--- app/src/main/java/com/example/rakshak/MainActivity.kt
+++ app/src/main/java/com/example/rakshak/MainActivity.kt
@@ -1,14 +1,71 @@
-package com.example.rakshak
-
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.example.rakshak.R
 
 class MainActivity : AppCompatActivity() {
 
+    private var emailTV: EditText? = null
+    private var passwordTV: EditText? = null
+    private var loginBtn: Button? = null
+    private var progressBar: ProgressBar? = null
+
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    updateTextButton.setOnClickListener { welcomeTextView.text = "Hello Kotlin World!" }
 
+        mAuth = FirebaseAuth.getInstance()
+
+        initializeUI()
+
+        loginBtn!!.setOnClickListener { loginUserAccount() }
+    }
+
+    private fun loginUserAccount() {
+        progressBar!!.visibility = View.VISIBLE
+
+        val email: String
+        val password: String
+        email = emailTV!!.text.toString()
+        password = passwordTV!!.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG).show()
+                    progressBar!!.visibility = View.GONE
+
+                  //  val intent = Intent(this.MainActivity, DashboardActivity::class.java)
+                   // startActivity(intent)
+                } else {
+                    Toast.makeText(applicationContext, "Login failed! Please try again later", Toast.LENGTH_LONG).show()
+                    progressBar!!.visibility = View.GONE
+                }
+            }
+    }
+
+    private fun initializeUI() {
+        emailTV = findViewById(R.id.email)
+        passwordTV = findViewById(R.id.password)
+
+        loginBtn = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+    }
 }
